657eb4827347b4572d84982696c6bc4dc971acb5
Robotechnic <robotechnic.developeur@gmail.com>
2020-06-28 14:29:34 +0200

    add javascript to get latest release json data

--- script/gitHubAPI.js
+++ script/gitHubAPI.js
@@ -1,3 +1,41 @@
+/*
+	CONFIGURATION
+*/
 const APIurl     = "https://api.github.com/"
 const user       = "Robotechnic"
 const repository = "entImager"
+
+const header = new Headers()
+header.append("Accept","application/vnd.github.v3+json")
+
+releasesRequest = new Request(APIurl+"repos/"+user+"/"+repository+"/releases",{
+	method:"GET",
+	headers:header
+})
+
+/*
+	FUNCTIONS
+*/
+getReleases = (callback) =>{
+	fetch(releasesRequest).then((result)=>{
+		result.json().then(callback)
+	})
+}
+
+/*
+	CODE
+*/
+var downloadContener = document.querySelector(".downloadContener")
+
+
+getReleases((jsonResult)=>{
+	if (jsonResult.message == "Not Found"){
+		downloadContener.style.color = "red"
+		downloadContener.style.display = 'flex'
+		downloadContener.style.justifyContent = "center"
+		downloadContener.innerHTML = "<h2>Releases not found</h2>"
+	} else {
+
+	}
+	console.log(jsonResult)
+})
